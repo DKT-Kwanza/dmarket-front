3778c0f4e30e1c3f2d3c7e4fbd48275747b9525a
hyeonyoung <ahy1612@naver.com>
2024-01-16 10:48:34 +0900

    [ui] #1 주문 조회 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import {Route, Routes} from "react-router-dom";
+
 import './App.css';
+import OrderHistory from './user/pages/MyPage/MyOrder/OrderHistory';
+import OrderHistoryDetail from "./user/pages/MyPage/MyOrder/OrderHistoryDetail";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Routes>
+          <Route path='/orderhistory' element={<OrderHistory/>}/>
+          <Route path='/orderhistorydetail' element={<OrderHistoryDetail/>}/>
+      </Routes>
     </div>
   );
 }
