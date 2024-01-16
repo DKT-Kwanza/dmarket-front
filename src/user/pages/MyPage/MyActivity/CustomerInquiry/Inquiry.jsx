68cd355182219875a6c92f1f2d640e36c7b4ef24
yejin <yejin331@daum.net>
2024-01-16 22:50:26 +0900

    [ui] #22 마이페이지 고객문의 리스트 답변 열고 닫기 수정

--- src/user/pages/MyPage/MyActivity/CustomerInquiry/Inquiry.jsx
+++ src/user/pages/MyPage/MyActivity/CustomerInquiry/Inquiry.jsx
@@ -1,17 +1,22 @@
-import './Inquiry.css';
+import React, { useState } from 'react';
 import MyPageSidebar from "../../../../components/MyPage/Sidebar/MyPageSidebar";
 import MyPageSubHeader from "../../../../components/MyPage/SubHeader/MyPageSubHeader";
-import { useNavigate } from 'react-router-dom';
+import './Inquiry.css';
 
 const Inquiry = () => {
 
-    const navigate = useNavigate();
-    const temp = () => {
-        navigate('/');
-    }
+    const [isExpanded1, setIsExpanded1] = useState(false);
+    const [isExpanded2, setIsExpanded2] = useState(false);
+
+    const handleToggle1 = () => {
+        setIsExpanded1(!isExpanded1);
+      };
+    
+      const handleToggle2 = () => {
+        setIsExpanded2(!isExpanded2);
+      };
 
     return (<>
-        <div><h1>Hi</h1></div>
         <div><MyPageSubHeader /></div>
         <div className='Inquiry-title'>
             <div className='Inquiry-title-bar'></div>
@@ -34,52 +39,58 @@ const Inquiry = () => {
                         <div className='Inquiry-contents-content-data-4'>답변 상태</div>
                     </div>
                     <ul className='Inquiry-contents-content-data-list'>
-
-                        <li className='Inquiry-contents-content-data-list-temp'>
+                        <li onClick={handleToggle1} className='Inquiry-contents-content-data-list-temp'>
                             <div className='Inquiry-contents-content-data-1'>주문/결제</div>
                             <div className='Inquiry-contents-content-data-2'>포인트 충전 어떻게 하나요?</div>
                             <div className='Inquiry-contents-content-data-3'>2014.01.08</div>
-                            <div className='Inquiry-contents-content-data-4'>답변 대기</div>
+                            <div className='Inquiry-contents-content-data-5'>답변 대기</div>
                         </li>
                         <hr className='Inquiry-contents-content-data-list-hr' />
-                        <div className='Inquiry-contents-content-data-list-detail'>
-                            <div className='Inquiry-contents-content-data-list-data'>
-                                <p>포인트 충전 어디서 하나요?</p>
-                                <p>여기에서 하면 되나요?</p>
+                        {isExpanded1 && (
+                            <div className='Inquiry-contents-content-data-list-detail'>
+                                <div className='Inquiry-contents-content-data-list-data'>
+                                    <p>포인트 충전 어디서 하나요?</p>
+                                    <p>여기에서 하면 되나요?</p>
+                                </div>
+                                <hr className='Inquiry-contents-content-data-list-hr' />
+                                <div className='Inquiry-contents-content-data-list-data'>
+                                    <p>답변 준비중입니다.</p>
+                                </div>
+                                <hr className='Inquiry-contents-content-data-list-hr' />
+                                <div className='Inquiry-contents-content-data-list-btn-wrap'>
+                                    <button onClick={handleToggle1} className='Inquiry-contents-content-data-list-btn'>확인</button>
+                                </div>
+                                <hr className='Inquiry-contents-content-data-list-btn-hr' />
                             </div>
-                            <hr className='Inquiry-contents-content-data-list-hr' />
-                            <div className='Inquiry-contents-content-data-list-data'>
-                                <p>답변 준비중입니다.</p>
-                            </div>
-                            <hr className='Inquiry-contents-content-data-list-hr' />
-                            <div className='Inquiry-contents-content-data-list-btn-wrap'>
-                                <button className='Inquiry-contents-content-data-list-btn' onClick={temp}>확인</button>
-                            </div>
-                            <hr className='Inquiry-contents-content-data-list-btn-hr' />
-                        </div>
-
-                        <li className='Inquiry-contents-content-data-list-temp' onClick={temp}>
+                         )}
+                        <li onClick={handleToggle2} className='Inquiry-contents-content-data-list-temp'>
                             <div className='Inquiry-contents-content-data-1'>반품/환불</div>
                             <div className='Inquiry-contents-content-data-2'>반품 신청했는데 언제 환불 되나요?</div>
                             <div className='Inquiry-contents-content-data-3'>2014.01.08</div>
-                            <div className='Inquiry-contents-content-data-4'>답변 완료</div>
+                            <div className='Inquiry-contents-content-data-6'>답변 완료</div>
                         </li>
                         <hr className='Inquiry-contents-content-data-list-hr' />
-                        <div className='Inquiry-contents-content-data-list-detail'>
-                            <div className='Inquiry-contents-content-data-list-data'>
-                                <p>포인트 충전 어디서 하나요?</p>
-                                <p>여기에서 하면 되나요?</p>
+                        {isExpanded2 && (
+                            <div className='Inquiry-contents-content-data-list-detail'>
+                                <div className='Inquiry-contents-content-data-list-data'>
+                                    <p>포인트 충전 어디서 하나요?</p>
+                                    <p>여기에서 하면 되나요?</p>
+                                </div>
+                                <hr className='Inquiry-contents-content-data-list-hr' />
+                                <div className='Inquiry-contents-content-data-list-data'>
+                                    <div className='Inquiry-contents-content-data-list-data-info'>
+                                        <div className='Inquiry-contents-content-data-list-data-info-profile'>관리자</div>
+                                        <div className='Inquiry-contents-content-data-list-data-info-date'>2024.01.08</div>
+                                    </div>
+                                    <div className='Inquiry-contents-content-data-list-data-answer'>비밀입니다.</div>
+                                </div>
+                                <hr className='Inquiry-contents-content-data-list-hr' />
+                                <div className='Inquiry-contents-content-data-list-btn-wrap'>
+                                    <button onClick={handleToggle2} className='Inquiry-contents-content-data-list-btn'>확인</button>
+                                </div>
+                                <hr className='Inquiry-contents-content-data-list-btn-hr' />
                             </div>
-                            <hr className='Inquiry-contents-content-data-list-hr' />
-                            <div className='Inquiry-contents-content-data-list-data'>
-                                <p>답변 준비중입니다.</p>
-                            </div>
-                            <hr className='Inquiry-contents-content-data-list-hr' />
-                            <div className='Inquiry-contents-content-data-list-btn-wrap'>
-                                <button className='Inquiry-contents-content-data-list-btn' onClick={temp}>확인</button>
-                            </div>
-                            <hr className='Inquiry-contents-content-data-list-btn-hr' />
-                        </div>
+                        )}
                     </ul>
                 </div>
             </div>
