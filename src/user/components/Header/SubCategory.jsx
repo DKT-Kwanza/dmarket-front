72fc17171a9a242faf310be3f128f535bb07398b
hyeonyoung <ahy1612@naver.com>
2024-01-17 14:47:31 +0900

    [ui] #21 서브 카테고리 작업

--- src/user/components/Header/SubCategory.jsx
+++ src/user/components/Header/SubCategory.jsx
@@ -1,48 +1,100 @@
 import './SubCategory.css'
 import {useState} from "react";
 
-const SubCategory = () =>{
+const SubCategory = ({onMouseLeave}) =>{
+    const [isHoverd, setIsHovered] = useState(true);
 
     return(
-        <div className='sub-sub-category-container'>
-            <div className='sub-sub-category-contents'>
-                <div className='sub-sub-category-contents-details'>
-                    <div className='sub-sub-category-contents-details-style'><button>여성 의류</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>남성 의류</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>유아 의류</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>신발</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>가방/잡화</button></div>
+    <div className='sub-sub-category-container' onMouseLeave={onMouseLeave}>
+        <div className='sub-sub-category-contents'>
+            <div className='sub-sub-category-contents-details'>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>여성 의류</button>
                 </div>
-                <div className='sub-sub-category-contents-details'>
-                    <div className='sub-sub-category-contents-details-style'><button>생필품</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>주방용품</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>메이크업</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>바디/헤어</button></div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>남성 의류</button>
                 </div>
-                <div className='sub-sub-category-contents-details'>
-                    <div className='sub-sub-category-contents-details-style'><button>가구</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>침구</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>인테리어</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>문구/사무용품</button></div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>유아 의류</button>
                 </div>
-                <div className='sub-sub-category-contents-details'>                    
-                    <div className='sub-sub-category-contents-details-style'><button>데스크탑/노트북</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>모바일/태블릿</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>영상가전</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>음향가전</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>주방가전</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>생활가전</button></div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>신발</button>
                 </div>
-                <div className='sub-sub-category-contents-details'>                    
-                    <div className='sub-sub-category-contents-details-style'><button>휘트니스</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>등산/수영</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>구기</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>골프</button></div>
-                    <div className='sub-sub-category-contents-details-style'><button>캠핑</button></div>
-                    <div ><button>자전거/기타레저</button></div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>가방/잡화</button>
                 </div>
             </div>
-          </div>
+            <div className='sub-sub-category-contents-details'>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>생필품</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>주방용품</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>메이크업</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>바디/헤어</button>
+                </div>
+            </div>
+            <div className='sub-sub-category-contents-details'>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>가구</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>침구</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>인테리어</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>문구/사무용품</button>
+                </div>
+            </div>
+            <div className='sub-sub-category-contents-details'>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>데스크탑/노트북</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>모바일/태블릿</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>영상가전</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>음향가전</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>주방가전</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>생활가전</button>
+                </div>
+            </div>
+            <div className='sub-sub-category-contents-details'>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>휘트니스</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>등산/수영</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>구기</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>골프</button>
+                </div>
+                <div className='sub-sub-category-contents-details-style'>
+                    <button>캠핑</button>
+                </div>
+                <div>
+                    <button>자전거/기타레저</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     )
 }
 export default SubCategory;
