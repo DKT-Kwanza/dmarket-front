4f9919eab6852b9dad630859770d25cac304e823
yejin <yejin331@daum.net>
2024-01-29 21:30:09 +0900

    [feat] #71 카테고리, 카테고리 별 상품 조회 연동

--- src/components/user/Header/Header.jsx
+++ src/components/user/Header/Header.jsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 import axios from "axios";
 import './Header.css'
-import SubCategory from './SubCategory'
 import user from '../../../assets/icons/user.svg'
 import heart from '../../../assets/icons/heart.svg'
 import shoppingBag from '../../../assets/icons/shoppingBag.svg'
@@ -11,8 +10,34 @@ import alert from '../../../assets/icons/alert.svg'
 function Header() {
     const navigate = useNavigate();
     const [isMainDivHovered, setMainDivHovered] = useState(false);
-    const [isSubDivHovered, setSubDivHovered] = useState(false);
     const [cartCount, setCartCount] = useState({});
+    const [categories, setCategories] = useState([]);
+    const [levelTwoCategories, setLevelTwoCategories] = useState([]);
+
+    const token = sessionStorage.getItem('token');
+    
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await axios.get('http://172.16.210.136:8080/api/products/categories', {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                if (response.data.code === 200) {
+                console.log(response.data)
+                setCategories(response.data.data);
+                console.log(categories)
+                const levelTwos = response.data.data.reduce((acc, curr) => [...acc, ...curr.child], []);
+                setLevelTwoCategories(levelTwos);
+                }
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+            };
+        
+            fetchCategories();
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,15 +61,6 @@ function Header() {
         navigate(`./mydkt/${menu}`);
     }
 
-    const handleMouseOver = () => {
-        setMainDivHovered(true);
-        setSubDivHovered(true);
-    }
-
-    const handleMouseLeaveSubCategory = () => {
-        setSubDivHovered(false);
-    }
-
     const navigateToMain = () => {
         navigate(`./`);
     }
@@ -52,6 +68,23 @@ function Header() {
     const navigateToCustomer = () => {
         navigate('./customer');
     }
+
+    /* 메인 카테고리에 마우스가 올라가면 상태를 true로 설정 */
+    const handleMouseEnterMain = () => {
+        setMainDivHovered(true);
+    };
+
+    /* 메인 카테고리에서 마우스가 벗어나면 상태를 false로 설정 */
+    const handleMouseLeaveMain = () => {
+        setMainDivHovered(false);
+    };
+
+
+    const navigateToCategory = (categoryId, categoryName1, categoryName2) => {
+        const encodedCategoryName1 = encodeURIComponent(categoryName1);
+        const encodedCategoryName2 = encodeURIComponent(categoryName2);
+        navigate(`/category/${categoryId}?category1depthName=${encodedCategoryName1}&category2depthName=${encodedCategoryName2}`);
+    };
 
     return (
         <div className="header-div">
@@ -61,8 +94,7 @@ function Header() {
                     <input className="search-box" placeholder='2023년 봄날 가벼워진 패션으로 나들이 가보자~'>
                     </input>
                     <div className='search-box-svg' onClick={navigateToSearch}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                             fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                             <path
                                 d="M11 19C12.775 18.9996 14.4988 18.4054 15.897 17.312L20.293 21.708L21.707 20.294L17.311 15.898C18.405 14.4997 18.9996 12.7754 19 11C19 6.589 15.411 3 11 3C6.589 3 3 6.589 3 11C3 15.411 6.589 19 11 19ZM11 5C14.309 5 17 7.691 17 11C17 14.309 14.309 17 11 17C7.691 17 5 14.309 5 11C5 7.691 7.691 5 11 5Z"
                                 fill="#505050"/>
@@ -76,8 +108,7 @@ function Header() {
                     <div className='bucket' onClick={() => navigateToPage('mycart')}>
                         <img src={shoppingBag}/>
                         <div className='bucket-count'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 21 20"
-                                 fill="none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 21 20" fill="none">
                                 <circle cx="10.8359" cy="10" r="10" fill="black"/>
                                 <text x="50%" y="50%" textAnchor="middle" dy=".3em" fill="white" font-size="12">
                                     {cartCount.cartCount}
@@ -98,27 +129,50 @@ function Header() {
                 </div>
             </div>
             <div className="category-div">
-                <div className="category-div-container" onMouseEnter={handleMouseOver}>
+                <div className="category-div-container" onMouseEnter={handleMouseEnterMain} onMouseLeave={handleMouseLeaveMain}>
                     <div className="main-category">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="14" viewBox="0 0 17 14"
-                             fill="none">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="14" viewBox="0 0 17 14" fill="none">
                             <rect x="0.835938" width="16" height="2" fill="black"/>
                             <rect x="0.835938" y="6" width="16" height="2" fill="black"/>
                             <rect x="0.835938" y="12" width="16" height="2" fill="black"/>
                         </svg>
                         <div className='category-text'>카테고리</div>
                     </div>
-                    <div className="sub-category">
-                        <button>패션의류/잡화</button>
-                        <button>뷰티/생필품</button>
-                        <button>홈데코/문구</button>
-                        <button>디지털/가전</button>
-                        <button>스포츠/건강</button>
+                    <div className='category-box'>
+                        <div className="sub-category">
+                        {categories.filter(cat => cat.categoryDepth === 1).map((category) => (
+                            <button 
+                                key={category.categoryId}
+                                className='sub-category-box-button'
+                            >
+                                {category.categoryName}
+                            </button>
+                        ))}
+                        </div>
+                        <div className='sub-category-box-whole'>
+                            <div className='sub-category-box-container'>
+                                {categories.filter(cat => cat.categoryDepth === 1).map((category, index) => (
+                                        <div className='sub-sub-category-contents-details'>
+                                            {isMainDivHovered && category.child.map((subCategory) => (
+                                                <div key={subCategory.categoryId} className='sub-sub-category-contents-details'>
+                                                    <div className='sub-sub-category-contents-details-style'>
+                                                        <button 
+                                                            onClick={() => navigateToCategory(subCategory.categoryId, category.categoryName, subCategory.categoryName)}
+                                                            className='sub-category-contents-details-button'
+                                                        >
+                                                            {subCategory.categoryName}
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                ))}
+                            </div>
+                        </div>
                     </div>
                     <div onClick={navigateToCustomer} className="user-center">고객센터</div>
                 </div>
             </div>
-            {isSubDivHovered && <SubCategory onMouseLeave={handleMouseLeaveSubCategory}/>}
         </div>
     );
 }
--- src/pages/user/Product/ProductPage.jsx
+++ src/pages/user/Product/ProductPage.jsx
@@ -1,30 +1,56 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './ProductPage.css'
 import ProductItem from '../../../components/user/Item/ProductItem';
 import Filter from '../../../components/user/Common/Filter/Filter';
 import Dropdown from '../../../components/user/Common/Select/Dropdown';
 import ScrollToTopBtn from "../../../components/user/Common/Button/ScrollToTopBtn";
+import { useLocation, useParams } from 'react-router-dom';
 
-function ProductList(){
+function ProductPage(){
     const [products, setProducts] = useState([]);
+    const location = useLocation();
+    const { categoryId } = useParams();
+    const [category1depthName, setCategory1depthName] = useState("");
+    const [category2depthName, setCategory2depthName] = useState("");
+
+    const token = sessionStorage.getItem('token');
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get("/api/ProductListData.json");
-                setProducts(response.data);
+                const response = await axios.get(`http://172.16.210.136:8080/api/products/categories/${categoryId}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                setProducts(response.data.data.productList);
             } catch (e) {
                 console.error("Error fetching data: ", e);
             }
         };
-        fetchData();
-    }, []);
+    
+        if (categoryId) {
+            fetchData();
+        }
+    
+        const queryParams = new URLSearchParams(location.search);
+        const category1depthName = queryParams.get('category1depthName');
+        const category2depthName = queryParams.get('category2depthName');
+    
+        if (category1depthName) {
+            setCategory1depthName(decodeURIComponent(category1depthName));
+        }
+        if (category2depthName) {
+            setCategory2depthName(decodeURIComponent(category2depthName));
+        }
+    }, [categoryId, location.search]);
+    
 
     return (
         <div className="productList-body">
-            <div className='productList-category'>홈데코 / 문구 / 가구</div>
-            <div className='productList-title'>가구</div>
+            <div className='productList-category'>{category1depthName} &#47; {category2depthName}</div>
+            <div className='productList-title'>{category2depthName}</div>
             <div className='productList-title-bar'></div>
             {/* //NOTE filter 컴포넌트로 분리했는데 별로면 주석 해제하세요 */}
             <Filter/>
@@ -35,12 +61,12 @@ function ProductList(){
                 {products.map((item, index) => (
                     <ProductItem 
                         key={index}
-                        imgSrc={item.imgSrc}
-                        brand={item.brand}
-                        productName={item.name}
-                        sales={item.price}
-                        ratingAvg={item.rating}
-                        reviewCnt={item.reviews}
+                        imgSrc={item.productImg}
+                        brand={item.productBrand}
+                        productName={item.productName}
+                        sales={item.productSalePrice}
+                        ratingAvg={item.productRating}
+                        reviewCnt={item.productReviewCount}
                     />
                 ))}
             </div>
@@ -49,4 +75,4 @@ function ProductList(){
     )
 }
 
-export default ProductList;
+export default ProductPage;
