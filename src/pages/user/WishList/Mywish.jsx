c4df8f97ea116a62fd5e6e0df3cc193c3c270a0c
hyeonyoung <ahy1612@naver.com>
2024-01-31 02:18:15 +0900

    [feat] #83 위시리스트 > 삭제

--- src/pages/user/WishList/Mywish.jsx
+++ src/pages/user/WishList/Mywish.jsx
@@ -6,6 +6,7 @@ import './Mywish.css';
 
 function WishList() {
     const [wishLists, setWishLists] = useState([]);
+    const [wishCount, setWishCount] = useState(0);
     const [selectAll, setSelectAll] = useState(false);
     const [checkedItems, setCheckedItems] = useState({});
 
@@ -24,6 +25,7 @@ function WishList() {
                     }
                 });
                 setWishLists(response.data.data);
+                setWishCount(response.data.data.wishCount);
                 console.log(response.data.data)
             } catch (e) {
                 console.error("Error fetching data: ", e);
@@ -32,29 +34,55 @@ function WishList() {
         fetchData();
     }, []);
 
-    const handleSelectAll = () => { // 전체 선택 체크박스 상태
+    /* 전체 선택 체크박스 상태 */
+    const handleSelectAll = () => {
         const newCheckedItems = {};
-        wishLists.forEach((item, index) => {
+        wishLists.wishListItem.forEach((item, index) => {
             newCheckedItems[index] = !selectAll;
         });
         setCheckedItems(newCheckedItems);
         setSelectAll(!selectAll);
     };
 
-    const handleItemCheck = index => { // 개별 상품 선택 체크박스 상태
+    /* 개별 상품 선택 체크박스 상태 */
+    const handleItemCheck = index => {
         const newCheckedItems = { ...checkedItems, [index]: !checkedItems[index] };
         setCheckedItems(newCheckedItems);
     };
 
-    const handleDeleteSelected = () => { // 체크한 상품 삭제
-        const newWishLists = wishLists.filter((_, index) => !checkedItems[index]);
-        setWishLists(newWishLists);
+    /* 위시 리스트 삭제 */
+    const handleDeleteSelected = async () => {
+        /* 선택된 상품들의 wishId를 추출 */
+        const selectedWishIds = wishLists.wishListItem
+            .filter((_, index) => checkedItems[index])
+            .map(item => item.wishId);
+
+        /* 새로운 위시 리스트 생성 */
+        const newWishLists = wishLists.wishListItem.filter((_, index) => !checkedItems[index]);
+        setWishLists({...wishLists, wishListItem: newWishLists});
 
         const newCheckedItems = {};
         newWishLists.forEach((_, index) => {
             newCheckedItems[index] = false;
         });
         setCheckedItems(newCheckedItems);
+
+        /* 선택된 상품들을 삭제하는 API 호출 */
+        try {
+            await Promise.all(selectedWishIds.map(async wishId => {
+                const url = `http://172.16.210.136:8080/api/users/${userId}/wish/${wishId}`;
+                await axios.delete(url, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                    }
+                });
+            }));
+            /* 선택된 상품의 개수 계산 */
+            const selectedItemCount = Object.values(checkedItems).filter(Boolean).length;
+            setWishCount(wishCount-selectedItemCount)
+        } catch (e) {
+            console.error("Error deleting Cart data: ", e);
+        }
     };
 
     return(
@@ -66,7 +94,7 @@ function WishList() {
                 <div className='wishList-checkbox'>
                     <CheckBox checked={selectAll} onChange={handleSelectAll} />
                 </div>
-                <div className='wishList-count'>전체 상품 : <span>{wishLists.wishCount}</span>개</div>
+                <div className='wishList-count'>전체 상품 : <span>{wishCount}</span>개</div>
                 <button onClick={handleDeleteSelected} className='wishList-delete-button'>삭제</button>
             </div>
             <div className='wishList-bar'></div>
