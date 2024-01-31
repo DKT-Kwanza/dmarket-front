066ac37caa4c6514b3dd740a43ac7eabf6928397
yejin <yejin331@daum.net>
2024-01-31 22:32:16 +0900

    [feat] #92 마이페이지 마일리지 연동

--- src/components/user/Item/MileageHistoryItem.jsx
+++ src/components/user/Item/MileageHistoryItem.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { formatPrice } from '../../../utils/Format';
 import './MileageHistoryItem.css'
 
 const MileageItem = ({ date, contents, addMileage, curMileage  }) => {
     // +, -에 따른 색상 차이
-    const mileageClass = addMileage.startsWith('+') 
-                         ? 'Mileage-contents-content-data-3-positive' 
-                         : 'Mileage-contents-content-data-3-negative';
-      
+    const mileageClass = addMileage < 0
+                     ? 'Mileage-contents-content-data-3-negative' 
+                     : 'Mileage-contents-content-data-3-positive';
+
     return (
         <ul className='Mileage-contents-content-data-list'>
             <li className='Mileage-contents-content-data-list-temp'>
                 <div className='Mileage-contents-content-data-1'>{date}</div>
                 <div className='Mileage-contents-content-data-2'>{contents}</div>
-                <div className={`Mileage-contents-content-data-3 ${mileageClass}`}>{addMileage}</div>
-                <div className='Mileage-contents-content-data-4'>{curMileage}</div>
+                <div className={`Mileage-contents-content-data-3 ${mileageClass}`}>{formatPrice(addMileage)}</div>
+                <div className='Mileage-contents-content-data-4'>{formatPrice(curMileage)}</div>
             </li>
             <hr className='Mileage-contents-content-data-list-hr' />
         </ul>
