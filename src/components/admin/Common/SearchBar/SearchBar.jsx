71380ef00ba6f837f83d9ef699f737e00cb6cc00
hyeonyoung <ahy1612@naver.com>
2024-02-01 18:03:57 +0900

    [feat] #97 사용자 검색 & 검색 후 삭제

--- src/components/admin/Common/SearchBar/SearchBar.jsx
+++ src/components/admin/Common/SearchBar/SearchBar.jsx
@@ -1,45 +1,35 @@
 import * as React from 'react';
-import { useState } from 'react';
+import {useState} from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import { useLocation, useNavigate } from 'react-router-dom';
 
-export default function SearchBar({text, handleSearchInput}) {
+export default function SearchBar({ text, onChange, onSearch, search }) {
 
-    const navigate = useNavigate();
-    //const location = useLocation();
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(search);
+    };
 
-    const [input, setInput] = useState();
-
-    // const handleSearch = (e) => {
-    //     if (e.key === 'Enter') {
-    //         const query = e.target.value;
-    //         navigate(`/memberMng/user?q=${encodeURIComponent(query)}`);
-    //         setSearchInput("");
-    //     }
-    // };
-    
     return (
         <Paper
             elevation={3}
             component="form"
-            sx={{ p: '2px 4px', mb: 4, display: 'flex', alignItems: 'center', width: '100%' }}
+            sx={{p: '2px 4px', mb: 4, display: 'flex', alignItems: 'center', width: '100%'}}
+            onSubmit={handleSubmit}
         >
-            <IconButton sx={{ p: '10px' }} aria-label="menu">
-                <MenuIcon />
+            <IconButton sx={{p: '10px'}} aria-label="menu">
+                <MenuIcon/>
             </IconButton>
             <InputBase
-                sx={{ ml: 1, flex: 1 }}
+                sx={{ml: 1, flex: 1}}
                 placeholder={text ? text : "상품명 검색"}
-                inputProps={{ 'aria-label': '상품명 검색' }}
-                //onKeyPress={handleSearch}
-                value={input}
-                onChange={(e) => handleSearchInput(e.target.value)}
+                inputProps={{'aria-label': '상품명 검색'}}
+                onChange={onChange}
             />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search" >
+            <IconButton type="button" sx={{p: '10px'}} aria-label="search">
                 <SearchIcon/>
             </IconButton>
         </Paper>
--- src/components/admin/Table/UserTable.jsx
+++ src/components/admin/Table/UserTable.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from 'react-router-dom';
 import {formatDate} from '../../../utils/Format';
 import Table from '@mui/material/Table';
@@ -26,33 +26,25 @@ const style = {
 };
 
 export default function UserTable({headers, rows, children}) {
-    const navigate = useNavigate();
-    const [isOpen, setIsOpen] = useState(false);
     const [selectedUserId, setSelectedUserId] = useState(null);
     const [row, setRow] = useState(rows);
     const token = sessionStorage.getItem('token');
 
-    const handleOpenModal = (userId) => {
-        setSelectedUserId(userId);
-        setIsOpen(true);
-    };
+    useEffect(() => {
+        setRow(rows);
+    }, [rows]);
 
-    const handleCloseModal = () => {
-        setIsOpen(false);
-    };
-
-    const handleConfirmDelete = async () => {
-        setIsOpen(false);
+    const handleConfirmDelete = async (userId) => {
         try {
             /* 삭제 API 호출 */
-            const response = await axios.delete(`http://172.16.210.136:8080/api/admin/users/${selectedUserId}`, {
+            const response = await axios.delete(`http://172.16.210.136:8080/api/admin/users/${userId}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                     'Content-Type': 'application/json; charset=UTF-8',
                 },
             });
             alert("해당 사용자를 삭제했습니다.");
-            setRow(row.filter(row => row.userId !== selectedUserId));
+            setRow(null);
         } catch (error) {
             console.log(selectedUserId);
             console.error('Delete API 호출 실패:', error);
@@ -60,48 +52,48 @@ export default function UserTable({headers, rows, children}) {
     }
 
     return (
-        <TableContainer component={Paper} sx={{mb: 2}}>
-            <Table sx={{minWidth: 650}} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        {
-                            headers.map((header) => (
-                                <TableCell>{header}</TableCell>
-                            ))
-                        }
-                    </TableRow>
-                </TableHead>
-                <TableBody>
+        <>
+            {(row &&
+                <TableContainer component={Paper} sx={{mb: 2}}>
+                    <Table sx={{minWidth: 650}} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                {
+                                    headers.map((header) => (
+                                        <TableCell>{header}</TableCell>
+                                    ))
+                                }
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
 
-                    <TableRow key={rows.userId} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
-                        <TableCell>{rows.userName}</TableCell>
-                        <TableCell>{rows.userDktNum}</TableCell>
-                        <TableCell>{rows.userEmail}</TableCell>
-                        <TableCell>{rows.userRole}</TableCell>
-                        <TableCell>{rows.userJoinDate}</TableCell>
-                        <TableCell>
-                            {
-                                !children
-                                    ?
-                                    <Button
-                                        variant="outlined"
-                                        color="error"
-                                        onClick={() => handleOpenModal(rows.userId)}
-                                    >
-                                        삭제
-                                    </Button>
-                                    :
-                                    <SelectBox text={'유형을 선택하세요'} options={['사용자', '총괄관리자', '시스템관리자', '상품관리자']}/>
-                            }
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-            {isOpen && (
-                <ConfirmCancelModal isOpen={isOpen} onClose={handleCloseModal} onConfirm={handleConfirmDelete}>
-                    <div>{rows.userName} 님의 계정을 삭제합니다.</div>
-                </ConfirmCancelModal>
+                            <TableRow key={row.userId} sx={{'&:last-child td, &:last-child th': {border: 0}}}>
+                                <TableCell>{row.userName}</TableCell>
+                                <TableCell>{row.userDktNum}</TableCell>
+                                <TableCell>{row.userEmail}</TableCell>
+                                <TableCell>{row.userRole}</TableCell>
+                                <TableCell>{row.userJoinDate}</TableCell>
+                                <TableCell>
+                                    {
+                                        !children
+                                            ?
+                                            <Button
+                                                variant="outlined"
+                                                color="error"
+                                                onClick={() => handleConfirmDelete(row.userId)}
+                                            >
+                                                삭제
+                                            </Button>
+                                            :
+                                            <SelectBox text={'유형을 선택하세요'}
+                                                       options={['사용자', '총괄관리자', '시스템관리자', '상품관리자']}/>
+                                    }
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             )}
-        </TableContainer>
+        </>
     );
 }
