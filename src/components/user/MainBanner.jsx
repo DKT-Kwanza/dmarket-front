bd5ac2f556bd493904e8b79a3deeea2d4ddcc70e
hyeonyoung <ahy1612@naver.com>
2024-02-08 18:17:46 +0900

    [add] #165 배너 제작 및 적용

--- src/components/user/MainBanner.jsx
+++ src/components/user/MainBanner.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { FaAngleLeft, FaAngleRight, FaPause, FaPlay } from "react-icons/fa";
+import { FaAngleLeft, FaAngleRight, FaPlay } from "react-icons/fa";
+import { IoIosPause, IoIosPlay } from "react-icons/io";
 import banner01 from '@assets/images/banner01.png'
 import banner02 from '@assets/images/banner02.png'
 import banner03 from '@assets/images/banner03.png'
+import banner04 from '@assets/images/banner04.png'
+import banner05 from '@assets/images/banner05.png'
 
 const MainBanner = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [pause, setPause] = useState(false);
-    const images = [banner01, banner02, banner03];
+    const images = [banner01, banner02, banner03, banner04, banner05];
 
     const nextImage = () => {
         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); 
@@ -37,9 +40,9 @@ const MainBanner = () => {
             </div>
             <div className='main-div-buttonwrapper'>
                 <button className='main-btn-movebtn' onClick={prevImage}><FaAngleLeft /></button>
-                <button>{currentIndex + 1}/{images.length}</button>
+                <div className='main-btn-text'>{currentIndex + 1} / {images.length}</div>
                 <button className='main-btn-movebtn' onClick={nextImage}><FaAngleRight /></button>
-                <button onClick={togglePause}>{pause ? <FaPlay /> : <FaPause />}</button>
+                <button className='main-btn-movebtn' onClick={togglePause}>{pause ? <IoIosPlay /> : <IoIosPause />}</button>
             </div>
         </div>
     );
