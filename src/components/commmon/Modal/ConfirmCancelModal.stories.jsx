c83482e9c140175770271135cee3a8d57a75e1bc
hyeonyoung <ahy1612@naver.com>
2024-02-13 17:54:17 +0900

    [add] #193 storybook component test

--- src/components/commmon/Modal/ConfirmCancelModal.stories.jsx
+++ src/components/commmon/Modal/ConfirmCancelModal.stories.jsx
@@ -6,13 +6,30 @@ export default {
   title: 'Modal/ConfirmCancel',
   component: ConfirmCancelModal,
   tags: ['autodocs'],
+  parameters: {
+    componentSubtitle:
+      '확인, 취소 버튼이 있는 modal 입니다.',
+    docs: { 
+      description: {
+        component:
+          `open modal 버튼을 클릭하면 state 를 변경 후 모달이 열립니다.`,
+      },
+    },
+  },
   argTypes: {
-    colors: ColorPalette,
+    children: {
+        description: 'modal 의 text 를 받습니다.',
+    },
+    colors: {
+        description: 'modal 확인, 취소 버튼의 색상을 결정합니다.',
+        default: '#3377FF',
+        type: ColorPalette,
+        control: 'color',
+    },
     onConfirm: { action: 'confirmed' },
     onClose: { action: 'closed' },
   },
 };
-
 const Template = (args) => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -23,12 +40,16 @@ const Template = (args) => {
     </>
   );
 };
-
+/**
+ * 기본 modal
+*/
 export const DefaultConfirmCancel = Template.bind({});
-ConfirmCancel.args = {
+DefaultConfirmCancel.args = {
   children: 'Are you sure you want to proceed?',
 };
-
+/**
+ * modal button의 색상 선택
+*/
 export const WithColor = Template.bind({});
 WithColor.args = {
   children: 'Are you sure you want to proceed?',
